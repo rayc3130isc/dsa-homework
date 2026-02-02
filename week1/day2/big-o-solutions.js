32244db02e1249764fc2e25e0192d9197b065c72
rayc3130isc <rchen9003@gmail.com>
2026-02-02 10:26:24 -0500

    Finished HW

--- week1/day2/big-o-solutions.js
+++ week1/day2/big-o-solutions.js
@@ -1,12 +1,20 @@
 // Question 1: O(n)
-// Question 2:
-// Question 3:
-// Question 4:
-// Question 5:
+// Question 2: O(n^2)
+// Question 3: O(a + b)
+// Question 4: O(n^2)
+// Question 5: O(n^2), space complexity is linear
 
 // Question 6:
 
-for (let i = 0; i < arr.length; i++) {
-	
-}
+const n = new Set();
 
+for (const value of arr) {
+	if (n.has(value)) {
+		return true;
+	} else {
+		n.add(value);
+	}
+}
+return false;
+}
+// this is my first time coding in JavaScript, so I had to look up some of the stuff
